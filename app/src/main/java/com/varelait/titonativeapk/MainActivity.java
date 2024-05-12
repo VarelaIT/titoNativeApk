f351b30f65b237807364795aed90e32d26456919
Ismael Varela <info@varelait.com>
2024-05-11 22:24:29 -0400

    self conseal keyboard

--- app/src/main/java/com/varelait/titonativeapk/MainActivity.java
+++ app/src/main/java/com/varelait/titonativeapk/MainActivity.java
@@ -3,10 +3,13 @@ package com.varelait.titonativeapk;
 import static java.lang.Float.parseFloat;
 import static java.lang.Integer.parseInt;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,7 +20,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.varelait.titonativeapk.logic.IMeasures;
 import com.varelait.titonativeapk.logic.Measures;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,7 +32,14 @@ public class MainActivity extends AppCompatActivity {
     public float height;
     public float base;
     public Button calcBtn;
+    public TextView railsOut;
+    public TextView lateralOut;
+    public TextView alfaOut;
+    public TextView jambasOut;
+    public TextView glassBaseOut;
+    public TextView glassHeightOut;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +58,25 @@ public class MainActivity extends AppCompatActivity {
                 calculate(v);
             }
         });
+
+        railsOut = (TextView) findViewById(R.id.railsOutput);
+        lateralOut = (TextView) findViewById(R.id.lateralOutput);
+        alfaOut = (TextView) findViewById(R.id.alfaisalOutput);
+        jambasOut = (TextView) findViewById(R.id.jambasOutput);
+        glassBaseOut = (TextView) findViewById(R.id.glassBaseOutput);
+        glassHeightOut = (TextView) findViewById(R.id.glassHeightOutput);
+    }
+
+    void renderResult (IMeasures result) {
+        DecimalFormat decimal = new DecimalFormat("#.##");
+        railsOut.setText((CharSequence) decimal.format(result.getRails()));
+        lateralOut.setText((CharSequence) decimal.format(result.getLaterals()));
+        alfaOut.setText((CharSequence) decimal.format(result.getAlfaisal()));
+        jambasOut.setText((CharSequence) decimal.format(result.getJambas()));
+        glassBaseOut.setText((CharSequence) decimal.format(result.getGlassBase()));
+        glassHeightOut.setText((CharSequence) decimal.format(result.getGlassHeigth()));
+        InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
     }
 
     void calculate (View view) {
@@ -52,12 +84,11 @@ public class MainActivity extends AppCompatActivity {
         getBaseInput();
         getHeightInput();
         getPanelInput();
-        TextView baseOutput = (TextView)findViewById(R.id.baseOutput);
 
         //Log.d("Tacker", "base: " + base + " height: " + height + " panels: " + panel );
         if (base > 10 && height > 10 && panel > 1) {
            measures = new Measures(base, height, panel);
-           baseOutput.setText((CharSequence)measures.getGlassBase().toString());
+           renderResult(measures);
         } else
             button.setText((CharSequence) "Error");
     }
